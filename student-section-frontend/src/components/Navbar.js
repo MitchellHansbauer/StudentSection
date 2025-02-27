20ff7908be5fc2499505758bc3c9041a3fad16d0
MajorSquax <47339129+MajorSquax@users.noreply.github.com>
2025-02-27 18:13:33 -0500

    Added Redis and Logout functionality

--- student-section-frontend/src/components/Navbar.js
+++ student-section-frontend/src/components/Navbar.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Logo from '../media/StudentSectionTransparent.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Navbar = () => {
+const Navbar = ({ handleLogout }) => {
+  const navigate = useNavigate();
+
+  const onLogoutClick = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:5000/logout", {
+        method: "POST",
+        credentials: "include", // Ensure cookies are sent with the request
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      if (response.ok) {
+        // Clear local storage
+        localStorage.removeItem("user");
+  
+        // Force reload to clear stored session cookies
+        window.location.href = "/login";
+      } else {
+        alert("Logout failed!");
+      }
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
+  
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
@@ -32,6 +57,11 @@ const Navbar = () => {
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/profile">Profile</Link>
+            </li>
+            <li className="nav-item">
+              <button className="btn btn-danger nav-link" onClick={onLogoutClick}>
+                Logout
+              </button>
             </li>
           </ul>
         </div>
